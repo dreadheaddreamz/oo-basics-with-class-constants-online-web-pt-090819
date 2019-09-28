8b271e8701f88fdf949dcf8cc77d33087f038415
James Batsell <jamesbatsell05@gmail.com>
2019-09-28 08:14:06 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,11 @@ class Shoe
   attr_accessor :color, :size, :material, :condition,
   attr_reader   :brand
 
-  def initialize(brand)
+   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
   end
   
   
@@ -14,10 +17,4 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-  
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
-
 end
